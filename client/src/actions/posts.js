f384d77fd446f9b821c61d22329b12a00e5f8bf5
Ameya Mahadev Gonal <ameyaagsilver@gmail.com>
2022-07-21 10:44:29 +0530

    Environment file added in server along with constants folder in client

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,9 +1,10 @@
 import * as api from '../api';
+import { FETCH_ALL, DELETE, CREATE, UPDATE, LIKE } from '../contants/actionTypes';
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        const action = {type: 'FETCH_ALL', payload: data};
+        const action = {type: FETCH_ALL, payload: data};
         dispatch(action);
     } catch (error) {
         console.log(error.message);
@@ -13,7 +14,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        const action = {type: 'CREATE', payload: data};
+        const action = {type: CREATE, payload: data};
         dispatch(action);
     } catch (error) {
         console.log(error.message);
@@ -23,7 +24,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
-        const action = {type: 'UPDATE', payload: data};
+        const action = {type: UPDATE, payload: data};
         dispatch(action);
     } catch (error) {
         console.log(error.message);
@@ -33,7 +34,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
-        const action = {type: 'DELETE', payload: id};
+        const action = {type: DELETE, payload: id};
         dispatch(action);
     } catch (error) {
         console.log(error.message);
@@ -45,7 +46,7 @@ export const likePost = (id) => async (dispatch) => {
         const { data } = await api.likePost(id);
         console.log("Liked dta")
         console.log(data);
-        const action = {type: 'LIKE', payload: data};
+        const action = {type: LIKE, payload: data};
         dispatch(action);
     } catch (error) {
         console.log(error.message);
--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const Connection = mongoose.connection;
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -15,7 +16,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ameyaagsilver:Ameya%402002@cluster0.cubj4re.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
